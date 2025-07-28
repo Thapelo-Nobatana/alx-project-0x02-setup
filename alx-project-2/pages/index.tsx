791ed08198ba52993fff52d173acb6ec92465901
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-28 10:28:11 +0200

    Initialized project using create-next-app with TypeScript, ESLint, and Tailwind CSS

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the landing page!</h1>
+    </div>
+  );
+}
