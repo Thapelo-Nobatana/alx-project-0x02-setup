cb47fdd24f7066d02c00c33fdf703c4043c0a677
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-07-28 13:09:50 +0200

    feat: add reusable Card component with title and content props

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,17 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC <CardProps>= ({ title, content}) => {
+  return (
+    <>
+    <div className="items-center">
+        <h1 className="text-xl font-bold">{title}</h1>
+        <div>
+            {content}
+        </div>
+    </div>
+    </>
+  );
+}
+
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,13 @@
+import Card from "@/components/common/Card";
 
 
-const myHome: React.FC = () => {
+const HomePage: React.FC = () => {
     return (
         <div>
             <h2>Home page</h2>
+            <Card title="This is a title for Card" content={["Kea", "This is the content", "There's not turning back"]} />
         </div>
     );
 }
 
-export default myHome;
+export default HomePage;
